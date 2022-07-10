264b9b423d1c7c726c7cc028da576ae085ed7a36
zhaoy <zhaoy@LAPTOP-6U9QOEV7>
2022-07-10 21:01:18 +0800

    修改图片地址

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,11 @@
 const products = [
   {
-    img: "/public/冰墩墩.png",
+    img: "/冰墩墩.png",
     name: "冰墩墩",
     price: 100,
   },
   {
-    img: "/public/雪容融.png",
+    img: "/雪容融.png",
     name: "雪容融",
     price: 50,
   },
